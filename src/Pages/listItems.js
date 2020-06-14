a32c75fe50d248df3f32eada89921ff7581bbe75
randel <randelfreitas@gmail.com>
2020-06-14 19:00:52 -0300

    Ajustando menu equipe

--- src/Pages/listItems.js
+++ src/Pages/listItems.js
@@ -22,7 +22,7 @@ export const mainListItems = (
       <ListItemIcon>
         <ShoppingCartIcon />
       </ListItemIcon>
-      <ListItemText primary="Orders" />
+      <ListItemText primary="Equipe" />
     </ListItem>
     <ListItem button>
       <ListItemIcon>
